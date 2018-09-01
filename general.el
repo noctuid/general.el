f1feeb8241bc724ced9952f328d6694329178cf1
Fox Kiester <noct@posteo.net>
2018-09-01 11:52:51 -0400

    Fix defcustom :prefix

--- general.el
+++ general.el
@@ -40,7 +40,7 @@
 (defgroup general nil
   "Gives convenient wrappers for key definitions."
   :group 'convenience
-  :prefix 'general-)
+  :prefix "general-")
 
 (defcustom general-implicit-kbd t
   "Whether to implicitly wrap a (kbd) around `general-define-key' keys.
